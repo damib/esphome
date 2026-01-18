499630ca2b392f6616c264ee7a0546c1507fc3fa
Damiano Bortolato <damiano.bortolato@lnl.infn.it>
2026-01-18 16:04:43 +0100

    Fixme

--- esphome/components/beko/beko.cpp
+++ esphome/components/beko/beko.cpp
@@ -25,6 +25,9 @@ bool BekoClimate::parse_state_frame_(const uint8_t frame[]) {
 }
 
 bool BekoClimate::on_receive(remote_base::RemoteReceiveData data) {
+  for(int i = 0; i < data.size(); i++) {
+    ESP_LOGD(TAG, "Received data[%d]: %d", i, data[i]);
+  }
   return false;
 }
 
