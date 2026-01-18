150d252b5f1f4fa0848ee667f369d2c6931248fc
Damiano Bortolato <damiano.bortolato@lnl.infn.it>
2026-01-18 16:00:07 +0100

    Fixme

--- esphome/components/beko/climate.py
+++ esphome/components/beko/climate.py
@@ -3,11 +3,10 @@ from esphome.components import climate_ir
 
 AUTO_LOAD = ["climate_ir"]
 
-daikin_ns = cg.esphome_ns.namespace("daikin")
-DaikinClimate = daikin_ns.class_("DaikinClimate", climate_ir.ClimateIR)
+beko_ns = cg.esphome_ns.namespace("beko")
+BekoClimate = beko_ns.class_("BekoClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(DaikinClimate)
-
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(BekoClimate)
 
 async def to_code(config):
     await climate_ir.new_climate_ir(config)
