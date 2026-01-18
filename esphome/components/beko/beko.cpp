6d7d2595280a0c0c35f97b6a4ef11405345ca4c0
Damiano Bortolato <damiano.bortolato@lnl.infn.it>
2026-01-18 17:10:09 +0100

    Fixme

--- esphome/components/beko/beko.cpp
+++ esphome/components/beko/beko.cpp
@@ -24,22 +24,20 @@ bool BekoClimate::parse_state_frame_(const uint8_t frame[]) {
   return true;
 }
 
+
+#define ntim 40
+
 bool BekoClimate::on_receive(remote_base::RemoteReceiveData data) {
   auto &raw = data.get_raw_data();
   int size = raw.size();
   int inx = 0;
-  char line[150];
-  while(size - inx > 20){
-    snprintf(line, 150, "% 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d % 4d",
-      raw[inx], raw[inx+1], raw[inx+2], raw[inx+3], raw[inx+4], raw[inx+5], raw[inx+6], raw[inx+7], raw[inx+8], raw[inx+9],
-      raw[inx+10], raw[inx+11], raw[inx+12], raw[inx+13], raw[inx+14], raw[inx+15], raw[inx+16], raw[inx+17], raw[inx+18], raw[inx+19]);
-      inx += 20;
+  char line[(ntim+1)*20];
+  while(inx < size){
+    for(int i = 0; i < ntim && inx < size; i++, inx++) {
+      snprintf(line+i*5, 150-i*5, " % 4d", data[inx]);
+    }
     ESP_LOGI(TAG, "%s", line);
   }
-  for(int i = 0; i < data.size()-inx; i++) {
-    snprintf(line+i*5, 150-i*5, " % 4d", data[inx+i]);
-  }
-  ESP_LOGI(TAG, "%s", line);
   return false;
 }
 
